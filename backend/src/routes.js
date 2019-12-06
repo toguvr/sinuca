1a79d8abe010986e9bdd16988aead9ff7e996f60
toguvr <augustotf93@gmail.com>
2019-12-05 21:47:38 -0300

    subindo com inclusao do timer no bd

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const multer = require ('multer')
 const uploadConfig = require('./config/upload')
 const BookingController = require('./controllers/BookingController')
 const TableController = require('./controllers/TableController')
+const TimerController = require('./controllers/TimerController') 
 
 const routes = express.Router()
 const upload = multer(uploadConfig)
@@ -12,5 +13,9 @@ routes.get('/table/:table_id',TableController.index)
 routes.put('/player/:table_id',TableController.edit)
 routes.put('/update/:table_id',BookingController.change)
 routes.put('/time/:table_id',BookingController.timer)
+
+routes.put('/timer/:table_id',TimerController.changeTime)
+routes.get('/timer',TimerController.getTime)
+routes.post('/timer/create',TimerController.store)
 
 module.exports = routes
